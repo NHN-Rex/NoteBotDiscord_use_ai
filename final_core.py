e9e8489c07398d405a8c627637b102981dbe578b
Nguyen Huu Nghia <nghia.nguyenhuunghia2003@gmail.com>
2025-06-02 03:49:31 +0700

    add repo_type

--- final_core.py
+++ final_core.py
@@ -13,9 +13,9 @@ from pathlib import Path
 
 checkpoint_path = Path(__file__).parent / "../models/final_model1"
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint_path.resolve(), local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_path.resolve(), local_files_only=True, repo_type='model')
 
-model = AutoModelForTokenClassification.from_pretrained(checkpoint_path, local_files_only=True)
+model = AutoModelForTokenClassification.from_pretrained(checkpoint_path, local_files_only=True, repo_type='model')
 
 labels = model.config.id2label
 
